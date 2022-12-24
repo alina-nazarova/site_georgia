1107f03fcaeb666acd98c1c1c04405a003ab5e9a
Alina <serdechkomamino@gmail.com>
2022-12-24 13:40:33 +0300

    added canvas

---
+++ canvas.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const image = new Image(60, 45); // Using optional size for image
+image.onload = drawImageActualSize; // Draw when image has loaded
+
+// Load an image of intrinsic size 300x227 in CSS pixels
+image.src = "images/index/Georgia_Flag_Cross_575705_300x200.jpg";
+
+function drawImageActualSize() {
+  // Use the intrinsic size of image in CSS pixels for the canvas element
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
+
+  // Will draw the image as 300x227, ignoring the custom size of 60x45
+  // given in the constructor
+  ctx.drawImage(this, 0, 0);
+}
