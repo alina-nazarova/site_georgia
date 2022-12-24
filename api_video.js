77980154de2753e7ad74017523eb4426f22fa216
Alina <serdechkomamino@gmail.com>
2022-12-24 14:21:31 +0300

    added api-video

---
+++ api_video.js
@@ -0,0 +1,33 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '315',
+        width: '560',
+        videoId: '1YiiYtTfCq0',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 120000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
